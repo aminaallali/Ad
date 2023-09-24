d383e85bb15027c24e15805ca5c8dbf10a1da597
Amina Allali <140288404+aminaallali@users.noreply.github.com>
2023-09-24 01:23:59 +0100

    Upy.py

--- py.py
+++ py.py
@@ -1,21 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
-from telegram import ParseMode
 from telegram.ext import Updater, CommandHandler
-import logging
-
-# Set up logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-# Get the logger instance
-logger = logging.getLogger(__name__)
 
 # Create an Updater instance with your bot token
-updater = Updater(token='YOUR_BOT_TOKEN', use_context=True)
+updater = Updater(token='6399081736:AAEietH_LsAD-ecun5z_7tpdlNZHtvC0z_Y', use_context=True)
 
 # Define a command handler for your bot
-def display_ad(update, context):
+def send_ad(update, context):
     chat_id = update.effective_chat.id
     try:
         # Get the Adsterra website content
@@ -29,19 +20,19 @@ def display_ad(update, context):
         iframe_tag['src'] = "//www.profitablecreativeformat.com/943b83cf68b5827f3584110a40cda053/invoke.js"
 
         # Send the ad message with the iframe tag
-        context.bot.send_message(chat_id=chat_id, text=str(iframe_tag), parse_mode=ParseMode.HTML)
-        
+        context.bot.send_message(chat_id=chat_id, text=str(iframe_tag))
+
         # Log the success
-        logger.info(f"Sent ad message to chat {chat_id}")
+        print(f"Sent ad message to chat {chat_id}")
     except Exception as e:
         # Handle the exception
-        logger.error(f"Failed to send ad message to chat {chat_id}: {e}")
+        print(f"Failed to send ad message to chat {chat_id}: {e}")
         # Send an error message
         context.bot.send_message(chat_id=chat_id, text="Sorry, something went wrong. Please try again later.")
 
 # Register the command handler with the dispatcher
 dispatcher = updater.dispatcher
-dispatcher.add_handler(CommandHandler('ad', display_ad))
+dispatcher.add_handler(CommandHandler('ad', send_ad))
 
 # Start the bot
 updater.start_polling()
